b853f0e3db4736d02d2f13c916021b1a374484c4
Mohamed Ichoubane <mohamedichoubanetata@gmail.com>
2025-05-03 23:43:43 +0100

    set up the frontend using react library

---
+++ frontend/jest.config.js
@@ -0,0 +1,11 @@
+export default {
+    testEnvironment: 'jsdom',
+    moduleNameMapper: {
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+    },
+    setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+    transform: {
+        '^.+\\.jsx?$': 'babel-jest'
+    },
+    moduleFileExtensions: ['js', 'jsx'],
+}
