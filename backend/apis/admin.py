79bc4a60477dce81d4a60528c0a1242d9cb159da
Mohamed Ichoubane <mohamedichoubanetata@gmail.com>
2025-05-03 18:32:21 +0100

    register the model in the admin interface

--- backend/apis/admin.py
+++ backend/apis/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Passenger
 
 # Register your models here.
+@admin.register(Passenger)
+class PassengerAdmin(admin.ModelAdmin):
+    list_display = ['passenger_id', 'survived', 'pclass', 'name', 'sex', 'age', 'sibsp', 'parch', 'ticket', 'fare', 'cabin', 'embarked']
