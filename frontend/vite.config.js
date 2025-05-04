ee08cb70ae5844467d47df7d383bb1418d28d8c5
Mohamed Ichoubane <mohamedichoubanetata@gmail.com>
2025-05-04 01:12:28 +0100

    set up Dockerfile, .dockerignore, and docker-compose.yml for frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,      // allows access from Docker
+    port: 5173,
+    strictPort: true,
+  },
 })
